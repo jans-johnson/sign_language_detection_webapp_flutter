b9b0f7260eb6cf48364b6c0b0036a0ba2b37ae85
Jans Johnson <jaansjohnson@gmail.com>
2023-04-24 14:43:09 +0530

    Minor change

--- lib/screens/screen_home.dart
+++ lib/screens/screen_home.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:js' as js;
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -51,9 +52,14 @@ class _ScreenHomeState extends State<ScreenHome> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        TextButton(onPressed: () {}, child: const Text("Menu Item")),
-                        TextButton(onPressed: () {}, child: const Text("Menu Item")),
-                        TextButton(onPressed: () {}, child: const Text("Menu Item")),
+                        TextButton(
+                            onPressed: () {
+                              js.context.callMethod('open', ['https://github.com/jans-johnson']);
+                            }, child: const Text("Github")),
+                        TextButton(
+                            onPressed: () {}, child: const Text("Menu Item")),
+                        TextButton(
+                            onPressed: () {}, child: const Text("Menu Item")),
                       ],
                     ),
                   ),
@@ -61,30 +67,43 @@ class _ScreenHomeState extends State<ScreenHome> {
                     child: Row(
                       children: [
                         Container(
-                          width: MediaQuery.of(context).size.width * 0.5,
+                          width: MediaQuery.of(context).size.width * 0.3,
                           child: Padding(
-                            padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.05),
+                            padding: EdgeInsets.only(
+                                left: MediaQuery.of(context).size.width * 0.05),
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 Text(
-                                    "Dynamic Sign Language Detection",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 42.0,
-                                      color: Colors.white,
-                                    ),),
-                                SizedBox(height: MediaQuery.of(context).size.width*0.01,),
+                                  "Dynamic Sign Language Detection",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 42.0,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height:
+                                      MediaQuery.of(context).size.width * 0.01,
+                                ),
                                 Text(
-                                    "The project aims to build a model that converts Indian Sign Language into corresponding words. We have used a skeletal-point feature extraction framework to identify hand landmarks from sequences containing distinct signs and use these landmarks to build a model for recognizing hand gestures using various Long Short-Term Memory (LSTM) Networks. This approach can produce an accurate result compared to the traditional approach. The user will be monitored  and using the machine learning techniques discussed above, which will perform the real-time translation to display the final result",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 15.0,
-                                      color: Color.fromARGB(185, 255, 255, 255),
-                                    ),),
+                                  "The project aims to build a model that converts Indian Sign Language into corresponding words. We have used a skeletal-point feature extraction framework to identify hand landmarks from sequences containing distinct signs and use these landmarks to build a model for recognizing hand gestures using various Long Short-Term Memory (LSTM) Networks. This approach can produce an accurate result compared to the traditional approach. The user will be monitored  and using the machine learning techniques discussed above, which will perform the real-time translation to display the final result",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.0,
+                                    color: Color.fromARGB(185, 255, 255, 255),
+                                  ),
+                                ),
                                 Spacer(),
                                 ElevatedButton(
-                                    onPressed: () {}, child: Text("Get Started")),
+                                    onPressed: () {},
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text("Get Started",
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                      ),),
+                                    )),
                               ],
                             ),
                           ),
@@ -93,7 +112,7 @@ class _ScreenHomeState extends State<ScreenHome> {
                             child: Container(
                           child: SvgPicture.asset(
                             "assets/home.svg",
-                            width: MediaQuery.of(context).size.width * 0.4,
+                            width: MediaQuery.of(context).size.width * 0.55,
                           ),
                         ))
                       ],
