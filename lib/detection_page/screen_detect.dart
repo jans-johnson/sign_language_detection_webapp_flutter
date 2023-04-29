adccc7a7e56f893f00851506abab77fe69dd189c
Jans Johnson <jaansjohnson@gmail.com>
2023-04-29 12:37:17 +0530

    Detection screen update

--- lib/detection_page/screen_detect.dart
+++ lib/detection_page/screen_detect.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 import 'dart:html';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:sign_language_detection_webapp_flutter/detection_page/DisplayCaptured.dart';
+import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 import 'package:sign_language_detection_webapp_flutter/theme/theme_config.dart';
+
+import '../utils/consts.dart';
 
 class ScreenDetect extends StatefulWidget {
   const ScreenDetect({super.key});
@@ -16,6 +18,7 @@ class _ScreenDetectState extends State<ScreenDetect> {
   List<CameraDescription>? cameras;
   CameraController? controller;
   XFile? image;
+  double _current = 0.0;
 
   @override
   void initState() {
@@ -42,42 +45,45 @@ class _ScreenDetectState extends State<ScreenDetect> {
 
   @override
   Widget build(BuildContext context) {
-
     void sendPicture(XFile picture) async {
-    final bytes = await picture.readAsBytes();
+      final bytes = await picture.readAsBytes();
 
-    final base64Image = base64Encode(bytes);
+      final base64Image = base64Encode(bytes);
 
-    final request = HttpRequest();
-    request.open('POST', 'http://127.0.0.1:5000/upload');
-    request.setRequestHeader('Content-Type', 'application/json;charset=utf-8');
+      final request = HttpRequest();
+      request.open('POST', 'http://127.0.0.1:5000/upload');
+      request.setRequestHeader(
+          'Content-Type', 'application/json;charset=utf-8');
 
-    request.onLoadEnd.listen((event) {
-    if (request.status == 200) {
-      print(request.response);
-    } else {
-      print('Failed to send image.');
+      request.onLoadEnd.listen((event) {
+        if (request.status == 200) {
+          print(request.response);
+        } else {
+          print('Failed to send image.');
+        }
+      });
+
+      request.send(jsonEncode({
+        'image': base64Image,
+      }));
+
+      await request.onLoadEnd.first;
     }
-  });
-
-    request.send(jsonEncode({
-      'image': base64Image,
-    }));
-
-    await request.onLoadEnd.first;
-  }
-
 
     Future<void> captureImage() async {
       try {
+        _current = 0.0;
         if (controller != null) {
           if (controller!.value.isInitialized) {
             for (int i = 0; i < 30; i++) {
               await controller!.takePicture().then((value) {
                 sendPicture(value);
                 image = value;
+                setState(() {
+                  _current++;
+                });
               });
-              await Future.delayed(const Duration(milliseconds: 20));
+              await Future.delayed(const Duration(milliseconds: 10));
               setState(() {
                 //update UI
               });
@@ -100,9 +106,10 @@ class _ScreenDetectState extends State<ScreenDetect> {
                 items: cameras != null
                     ? cameras!.map((e) {
                         return DropdownMenuItem(
-                          child: Text(e.name,style: TextStyle(
-                            color: Color.fromARGB(255, 75, 75, 75),
-                          )),
+                          child: Text(e.name,
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 75, 75, 75),
+                              )),
                           value: e,
                         );
                       }).toList()
@@ -120,6 +127,21 @@ class _ScreenDetectState extends State<ScreenDetect> {
                   });
                 }),
             SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child: Text(
+                Constants.instructions,
+                overflow: TextOverflow.clip,
+                style: TextStyle(
+                    color: ThemeConfig.lightPrimary,
+                    fontSize: MediaQuery.of(context).textScaleFactor * 15,
+                    height: MediaQuery.of(context).textScaleFactor * 1.5,
+                    wordSpacing: MediaQuery.of(context).textScaleFactor * 2),
+              ),
+            ),
+            SizedBox(
                 height: 350,
                 width: 600,
                 child: controller == null
@@ -129,11 +151,31 @@ class _ScreenDetectState extends State<ScreenDetect> {
                             child: CircularProgressIndicator(),
                           )
                         : CameraPreview(controller!)),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
             ElevatedButton.icon(
-                onPressed: captureImage,
-                icon: Icon(Icons.camera),
-                label: Text("Capture")),
-            DisplayCaptured(image: image)
+              onPressed: captureImage,
+              icon: Icon(Icons.camera),
+              label: Text("Capture"),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: FAProgressBar(
+                currentValue: _current,
+                maxValue: 29,
+                changeColorValue: 100,
+                backgroundColor: Colors.white,
+                progressColor: Colors.lightBlue,
+                direction: Axis.horizontal,
+                verticalDirection: VerticalDirection.up,
+                displayText: 'Images Completed',
+                formatValueFixed: 2,
+              ),
+            ),
           ],
         ),
       ),
--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -7,5 +7,8 @@ class Constants {
 communication between society's hearing and non-hearing sections. The ML model makes its predictions based on 
 action recognition using skeletal points. Each sign has a distinct set of skeletal points, which can be 
 accurately defined using MediaPipe. The application supports real-time translation and identifies 25 alphabets 
-which high accuracy. The interface is created using Flutter.'''.replaceAll("\n", "");
+which high accuracy. The interface is created using Flutter.'''
+          .replaceAll("\n", "");
+  static String instructions = '''Make Sure that you are in a well-lit room and the hands are completely visible in the frame. 
+Click the button below to start capturing 30 frames.'''.replaceAll("\n", "");
 }
